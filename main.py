352973c644f44ee6eeb03d4a81f441aa2189809b
Lucaslive974 <lucasribeirolima974@gmail.com>
2023-12-07 19:16:06 -0300

    Fix DB export

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 from Interfaces.Extractor import Extractor
 from Classes.AutoInfracao import AutoInfracao
 import pyodbc
-from datetime import datetime
 
 def main():
     pdf_path = 'Testes/Auto com veiculo.pdf'
@@ -24,33 +23,31 @@ def main():
 
     # Converte para JSON
     json_dump = json.dumps(auto_infracao_list, indent=2, ensure_ascii=False, default=str)
-    json_data = json.loads(json_dump)
-    print(json_data[])
-   
+    json_data = json.loads(json_dump)   
    
     # Connect to SQL Server
     conn = pyodbc.connect('DRIVER={SQL Server};'
-                        'SERVER=172.22.0.15;'
+                        'SERVER=192.168.0.11,32771;'
                         'DATABASE=bonfire;'
                         'UID=sa;'
-                        'PWD=password')
+                        'PWD=teste12345')
 
     cursor = conn.cursor()
 
     cursor.execute('''
         INSERT INTO auto_infracao (
-            linha, veiculo, placa, numauto, concessionaria, data, local,
-            baselegal, codinfracao, dispositivo, descricao, observacao, agente,
-            pontuacao, dataemissao, datalimrecurso, valormulta
+            linha, veiculo, placa, num_auto, concessionaria, data, local,
+            base_legal, cod_infracao, dispositivo, descricao, observacao, agente,
+            pontuacao, data_emissao, data_lim_recurso, valor_multa
         )
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     ''', 
-    json_data[0], json_data[1], json_data[2], json_data[3],
-    json_data[4], json_data[5],
-    json_data[6], json_data[7], json_data[8],
-    json_data[9], json_data[10], json_data[11],
-    json_data[12], json_data[13],
-    json_data[14], json_data[15], json_data[17])
+    json_data[0]['linha'], json_data[0]['veiculo'], json_data[0]['placa'], json_data[0]['numauto'],
+    json_data[0]['concessionaria'], json_data[0]['data'], json_data[0]['local'],
+    json_data[0]['baselegal'], json_data[0]['codinfracao'], json_data[0]['dispositivo'],
+    json_data[0]['descricao'], json_data[0]['observacao'], json_data[0]['agente'],
+    json_data[0]['pontuacao'], json_data[0]['dataemissao'], json_data[0]['datalimrecurso'],
+    json_data[0]['valormulta'])
 
     # Commit the transaction
     conn.commit()
