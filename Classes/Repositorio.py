7a7c21c9e891d0dec798324768433b0d4b99fc45
lucas.lima <lucasribeirolima974@gmail.com>
2023-12-11 16:30:49 -0300

    Refactor: Extract Repositorie responsability to a class

---
+++ Classes/Repositorio.py
@@ -0,0 +1,46 @@
+import json
+import pyodbc
+
+class Repositorio:
+    def __init__(self, config_path='Config/db_config.json'):
+        self.config = self.load_config(config_path)
+        self.connection = self.get_database_connection()
+
+    def load_config(self, config_path):
+        with open(config_path, 'r') as config_file:
+            return json.load(config_file)
+
+    def get_database_connection(self):
+        db_config = self.config.get("database", {}) 
+        driver = db_config.get('driver')
+        server = db_config.get('server')
+        database = db_config.get('database')
+        uid = db_config.get('uid')
+        pwd = db_config.get('pwd')
+
+        if None in [driver, server, database, uid, pwd]:
+             raise ValueError("Algumas configurações do banco de dados estão ausentes ou configuradas incorretamente.")
+
+        db_config = self.config.get("database", {})
+        connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={uid};PWD={pwd}"
+
+        return pyodbc.connect(connection_string)
+
+    def insert_auto_infracao(self, data):
+        query = '''
+            INSERT INTO auto_infracao (
+                linha, veiculo, placa, num_auto, concessionaria, data, local,
+                base_legal, cod_infracao, dispositivo, descricao, observacao, agente,
+                pontuacao, data_emissao, data_lim_recurso, valor_multa
+            )
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        '''
+
+        with self.connection.cursor() as cursor:
+            cursor.execute(query, tuple(data.values()))
+
+        # Commit the transaction
+        self.connection.commit()
+
+    def close_connection(self):
+        self.connection.close()
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import PyPDF2
-import pyodbc
 from Interfaces.Extractor import Extractor
 from Classes.AutoInfracao import AutoInfracao
+from Classes.Repositorio import Repositorio
 
 def main():
     pdf_path = 'Testes\\Autos completo.pdf'
     auto_infracao_list = []
+    repositorio = Repositorio();
 
+    
     with open(pdf_path, 'rb') as pdf_file:
         pdf_reader = PyPDF2.PdfReader(pdf_file)
 
@@ -20,36 +22,15 @@ def main():
             # Adiciona à lista
             auto_infracao_list.append(auto_infracao.to_dict())
 
-    conn = pyodbc.connect('DRIVER={SQL Server};'
-                        'SERVER=192.168.0.1;'
-                        'DATABASE=bonfire;'
-                        'UID=sa;'
-                        'PWD=password')
+    try:
+        for auto_infracao_data in auto_infracao_list:
+            repositorio.insert_auto_infracao(auto_infracao_data)
 
-    for i, item in enumerate(auto_infracao_list):
+    except Exception as e:
+        print(f"Um erro ocorreu: {e}")
 
-        cursor = conn.cursor()
-        
-        cursor.execute('''
-            INSERT INTO auto_infracao (
-                linha, veiculo, placa, num_auto, concessionaria, data, local,
-                base_legal, cod_infracao, dispositivo, descricao, observacao, agente,
-                pontuacao, data_emissao, data_lim_recurso, valor_multa
-            )
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', 
-
-        auto_infracao_list[i]["linha"], auto_infracao_list[i]["veiculo"], auto_infracao_list[i]["placa"], auto_infracao_list[i]["numauto"],
-        auto_infracao_list[i]["concessionaria"], auto_infracao_list[i]["data"], auto_infracao_list[i]["local"], auto_infracao_list[i]["baselegal"],
-        auto_infracao_list[i]["codinfracao"], auto_infracao_list[i]["dispositivo"], auto_infracao_list[i]["descricao"], auto_infracao_list[i]["observacao"],
-        auto_infracao_list[i]["agente"], auto_infracao_list[i]["pontuacao"], auto_infracao_list[i]["dataemissao"], auto_infracao_list[i]["datalimrecurso"],
-        auto_infracao_list[i]["valormulta"])
-
-        # Commit the transaction
-        conn.commit()
-
-    # Close the connection
-    conn.close() 
+    finally:
+        repositorio.close_connection()
 
 if __name__ == "__main__":
     main()
