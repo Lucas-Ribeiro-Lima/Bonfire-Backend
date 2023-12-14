bb4019bfc94374d0d23f0b6dfd89ec49799ea6fc
lucas.lima <lucasribeirolima974@gmail.com>
2023-12-14 10:15:33 -0300

    Basic structure of Frontend - Next.js

---
+++ frontend/bonfire/src/app/importAuto/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Sidebar from '../../../components/Sidebar'
+
+export default function Home() {
+    return (
+        <div className="flex min-h-screen">
+          <Sidebar />
+          {/* Conteúdo principal da página aqui */}
+        </div>
+    )
+}
