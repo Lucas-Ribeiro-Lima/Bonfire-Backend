878d8bfcdb9b72cbfc07568fdb9335d0f930de38
lucas.lima <lucasribeirolima974@gmail.com>
2023-12-15 10:46:43 -0300

    Bonfire Frontend redesigner - initial commit

--- frontend/bonfire/src/app/page.tsx
+++ frontend/bonfire/src/app/page.tsx
@@ -1,113 +1,64 @@
-import Image from 'next/image'
+import Link from "next/link"
+import { AlignJustify, Flame, LucideHome, LucideImport, LucideSunMoon, Settings, Undo, X, XCircle } from "lucide-react"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className='h-screen flex flex-col'>
+        <header className='bg-gradient-to-r from-zinc-900 to-zinc-700 h-20 p-2 align-center border-b border-white/25'>
+          <div className="cursor-pointer w-40">
+            <h1 className="flex flex-row text-extrabold text-3xl text-red-800 items-center"><Flame></Flame>Bonfire</h1>
+            <h2 className="text-bold text-zinc-400">Autos de Infração</h2>
+          </div>
+          <div className="absolute right-10 top-6 text-zinc-400 hover:text-white hover:duration-1000 cursor-pointer">
+            <LucideSunMoon></LucideSunMoon>
+          </div>
+        </header>
+        <div className='flex flex-1'>
+          <aside className='w-64'>
+            <nav className="flex flex-col gap-4 bg-zinc-700 w-max p-4 text-zinc-500 rounded-br-lg">
+              <Link className="flex gap-2  hover:text-white hover:duration-1000" href="#" ><LucideHome></LucideHome>Inicio</Link>
+              <Link className="flex gap-2  hover:text-white hover:duration-1000" href="#" ><AlignJustify></AlignJustify>Cadastro</Link>
+              <Link className="flex gap-2  hover:text-white hover:duration-1000" href="#" ><LucideImport></LucideImport>Importação</Link>
+              <Link className="flex gap-2  hover:text-white hover:duration-1000" href="#" ><Settings></Settings>Configuração</Link>
+              <Link className="flex gap-2  hover:text-white hover:duration-1000" href="#" ><Undo></Undo>Sair</Link>
+            </nav>
+          </aside>
+          <main className='flex flex-col flex-1 bg-zinc-700 rounded-md m-6 relative right-12 text-white/70 p-6'>
+            <div className="flex flex-row justify-center items-center">
+              <h1 className="text-bold text-2xl p-4">Importação de Infrações</h1>
+              {/* <XCircle className='text-zinc-800 cursor-pointer hover:text-white/80 hover:duration-1000'></XCircle> */}
+            </div>
+            <div className="flex bg-zinc-500 w-max justify-center rounded p-4">
+              <form className="flex flex-col mt-10 gap-4">
+                Selecione o arquivo:
+                <input type='file' className='rounded-lg'></input>
+                <div className="flex flex-row gap-4">
+                <label className='flex gap-2'>
+                  <input type='checkbox' name='Instancia' value='1Instancia'></input>
+                  1° Instância
+                </label>
+                <label className="flex gap-2">
+                  <input type='checkbox' name='Instancia' value='2Instancia'></input>
+                  2° Instância
+                </label>
+                </div>
+                <button type='submit' className="bg-zinc-200 w-max rounded-lg text-black p-1 hover:bg-white">Importar</button>
+              </form>
+            </div>
+          </main>
+        </div>
+        <div>
+          <footer className='flex bg-gradient-to-l from-zinc-900 to-zinc-700 h-20 align-center items-center border-t border-white/25 justify-end pr-4 text-white/70'>
+            <div className="flex flex-col items-center text-sm">
+              <div>
+                Desenvolvido por
+              </div>
+              <div>
+              Guilherme Nogueira e Lucas Ribeiro
+              </div>
+            </div>
+          </footer>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
