86cb578c29d3ea7a06341629a155d0b923ef3ea8
Lucaslive974 <lucasribeirolima974@gmail.com>
2023-12-16 18:10:45 -0300

    Login screen designer update

--- frontend/bonfire/components/loginLayout.tsx
+++ frontend/bonfire/components/loginLayout.tsx
@@ -1,31 +1,48 @@
+import { LockIcon } from "lucide-react";
 import Footer from "./footer";
 import Logo from "./logo";
+import Link from "next/link";
 
 
 const LoginForm = () => {
     return (
-        <form className="flex flex-col gap-4 items-center">
-            <label className="flex flex-row gap-4">
-                Username:
-                <input type="text" className="w-46 text-black rounded-lg" ></input>
-            </label>
-            <label className="flex flex-row gap-4">
-                Password:
-                <input type="password" className="relative left-1 w-46 text-black rounded-lg" ></input>
-            </label>
-            <label className="flex gap-2 relative right-10 pt-2 text-sm">
-                <input type="checkbox"></input>
-                Permanecer Conectado
-            </label>
-            <button type="submit" className="relative left-24 top-4 text-black bg-zinc-400 pl-2 pr-2 pt-1 pb-1 rounded-lg hover:bg-white hover:duration-1000 hover:shadow-2xl hover:shadow-black">Log-in</button>
+        <form className="flex flex-col items-center gap-4">
+            <div className="relative right-20 text-xl">
+                Sign in to your account
+            </div>
+            <div className="flex flex-col rounded-md shadow-sm -space-y-px">
+                <label>
+                    <input type="text" placeholder="Username" 
+                     className="w-96 rounded-t-md border border-gray-300 text-black indent-2 focus:outline-none focus:bg-zinc-300" ></input>
+                </label>
+                <label>
+                    <input type="password" placeholder="Password" 
+                     className="w-96 rounded-b-md border border-gray-300 text-black indent-2 focus:outline-none focus:bg-zinc-300" ></input>
+                </label>
+            </div>
+            <div className="flex flex-row gap-12 text-sm">
+                <label className="flex gap-2">
+                    <input type="checkbox"></input>
+                    Remember me
+                </label>
+                <Link href="#" className="text-red-800">
+                    Forgot password?
+                </Link>
+            </div>
+            <button type="submit" className="flex flex-row gap-2 w-5/6 h-8 justify-center items-center text-black bg-zinc-400 rounded-lg hover:bg-red-800 hover:duration-1000 hover:shadow-2xl hover:shadow-black">
+                <LockIcon></LockIcon>
+                Sign in
+            </button>
         </form>
     );
 }
 
 const LoginMenu = () => {
     return(
-        <div className="flex flex-col items-center gap-12 pt-8 bg-zinc-700 h-2/4 w-1/4 rounded-3xl shadow-inner shadow-black/50">
-            <Logo></Logo>
+        <div className="flex flex-col items-center gap-16">
+            <div className="pl-5 scale-150">
+                <Logo></Logo>
+            </div>
             <LoginForm></LoginForm>
         </div>
     );
@@ -33,8 +50,8 @@ const LoginMenu = () => {
 
 const LoginLayout = () => {
     return(
-        <div className="flex flex-col h-screen">            
-            <div className="flex flex-1 justify-center items-center">
+        <div className="flex flex-col h-screen bg-gradient-to-r from-zinc-900 to-zinc-800">            
+            <div className="flex flex-1 justify-center items-center relative bottom-10">
                 <LoginMenu></LoginMenu>
             </div>
             <div>
