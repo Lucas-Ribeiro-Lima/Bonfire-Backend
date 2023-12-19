9e644df67280c157af2cde95cbc72e03389b2be7
lucas.lima <lucasribeirolima974@gmail.com>
2023-12-19 15:59:00 -0300

    Axios API POST primeiraInstancia

--- frontend/bonfire/services/apiClient.tsx
+++ frontend/bonfire/services/apiClient.tsx
@@ -3,11 +3,11 @@ import axios from "axios";
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 const apiPort = process.env.NEXT_PUBLIC_API_PORT;
 
-const ApiClient = axios.create({
-    baseURL: `${apiUrl}:${apiPort}/`,
-    timeout: 1000,
+export const ApiClient = axios.create({
+    // baseURL: `${apiUrl}:${apiPort}/`,
+    baseURL: "http://127.0.0.1:5000/autoInfracao/",
+    timeout: 5000,
     headers: {
         'X-Custom-Header': 'foobar',
     }
 });
-
