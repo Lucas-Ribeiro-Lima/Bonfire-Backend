bac89ee74e750db1cc4b4388e74f17a2a4570e3b
lucas.lima <lucasribeirolima974@gmail.com>
2023-12-27 16:46:58 -0300

    Dropdown component added

---
+++ frontend/bonfire/src/app/registers/vehicles/page.tsx
@@ -0,0 +1,10 @@
+import MainApp from "@/components/mainApp";
+import PrimaryLayout from "@/components/primaryLayout";
+
+export default function Home() {
+    return (
+        <PrimaryLayout>
+            <MainApp title="Veiculos">Teste</MainApp>
+        </PrimaryLayout>
+    )
+}
