07aea2afcf38ba117adf97587a2cecd65a1dc6fa
Lucas Ribeiro Lima <lucaslive974@outlook.com>
2025-07-11 14:25:05 -0300

    feature: Add command line argument parsing for debug mode and ports

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-from flask import Flask
-from routes import autoInfracaoPrimeiraInstancia, autoInfracaoSegundaInstancia, veiculos, linha, consorcio
-from flask_cors import CORS
+from argparse import ArgumentParser
 from waitress import serve
+from flask import Flask
+from flask_cors import CORS
+from routes import autoInfracaoPrimeiraInstancia, autoInfracaoSegundaInstancia, veiculos, linha, consorcio
 
-from routes import consorcio
+parser = ArgumentParser()
+parser.add_argument("--debug", action="store_true", help="Debug mode")
+parser.add_argument("--port", type=int, default=5000, help="Server port")
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
+args = parser.parse_args()
 
-    # Registra o blueprint 'main'
-    app.register_blueprint(autoInfracaoPrimeiraInstancia.autoInfracaoPrimeiraInstanciaBlueprint)
-    app.register_blueprint(autoInfracaoSegundaInstancia.autoInfracaoSegundaInstanciaBlueprint)
-    app.register_blueprint(veiculos.veiculoBlueprint) 
-    app.register_blueprint(linha.linhaBlueprint)
-    app.register_blueprint(consorcio.consorcioBlueprint)
+app = Flask(__name__)
+CORS(app)
 
-    return app
+# Registra o blueprint 'main'
+app.register_blueprint(autoInfracaoPrimeiraInstancia.autoInfracaoPrimeiraInstanciaBlueprint)
+app.register_blueprint(autoInfracaoSegundaInstancia.autoInfracaoSegundaInstanciaBlueprint)
+app.register_blueprint(veiculos.veiculoBlueprint) 
+app.register_blueprint(linha.linhaBlueprint)
+app.register_blueprint(consorcio.consorcioBlueprint)
 
-if __name__ == "__main__":
-    app = create_app()
-    # app.run(debug=True)
-    serve(app, host="0.0.0.0", port=5000)
+if args.debug:
+    app.run(debug=True, port=args.port)
+else:
+    serve(app, host="0.0.0.0", port=args.port)
