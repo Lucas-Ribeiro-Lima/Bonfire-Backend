46125bd000a31272bb2fdd8c6abdc4902e4dd2df
lucas.lima <lucasribeirolima974@gmail.com>
2025-10-10 15:14:06 -0300

    Feat: Add a terminal output for the logger class together with the file write.

--- handlers/log.py
+++ handlers/log.py
@@ -5,8 +5,10 @@ def HandleErrorLog(exception: Exception):
   """Realiza a gravação da stack completa de erros no arquivo de log"""
   bonfireLogPath = 'Logs/bonfire-error.log'
   try:
+    date_str = datetime.now()
+    print(f"\n{date_str} - {exception}")
     with open(bonfireLogPath, "a") as logFile:                
-      logFile.writelines(f"\n{datetime.now()} - {exception}")
+      logFile.writelines(f"\n{date_str} - {exception}")
   except Exception as e:
       raise ErrLogger("Error ao salvar no arquivo de log", 500)
   
@@ -14,7 +16,9 @@ def HandleSuccessLog(message: str):
   """Realiza a gravação de mensagens de sucesso no arquivo de log"""
   bonfireLogPath = 'Logs/bonfire-sucess.log'
   try:
+    date_str = datetime.now()
+    print(f"\n{date_str} - {message}")
     with open(bonfireLogPath, "a") as logFile:                
-      logFile.writelines(f"\n{datetime.now()} - {message}")
+      logFile.writelines(f"\n{date_str} - {message}")
   except Exception as e:
       raise ErrLogger("Error ao salvar no arquivo de log", 500)
