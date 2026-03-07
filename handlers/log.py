4a50936c7056051fbf35ba6f453cf968533965f1
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-03-07 10:13:05 -0300

    feat: Logger segregate the messages by day

--- handlers/log.py
+++ handlers/log.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 
-bonfireLogPath = 'log/bonfire.log'
+bonfireLogPath = 'log/bonfire'
+
+def concatenateLogFileName(date: datetime):
+    return f"{bonfireLogPath}-{date.strftime("%d-%m-%Y")}.log"
   
 def writeToLogFile(message: str | Exception):
   """Realiza a gravação de mensagem no arquivo de log"""
   try:
-    date_str = datetime.now()
-    print(f"{date_str} - {message}")
-    with open(bonfireLogPath, "a") as logFile:                
-      logFile.writelines(f"{date_str} - {message}\n")
+    date = datetime.now()
+    print(f"{date.strftime('%d-%m-%Y %H:%M:%S')} - {message}")
+    with open(concatenateLogFileName(date), "a") as logFile:
+      logFile.writelines(f"{date.strftime('%H:%M:%S')} - {message}\n")
 
   except Exception as _:
         print("WARN:: Error ao salvar no arquivo de log")
