af8fe981e7a0795b4a834147c4b056027260024b
Lucas-Ribeiro-Lima <lucasribeirolima974@gmail.com>
2026-03-07 19:07:44 -0300

    refactor: Move app initialization to an unique module

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-from flask import Flask
-from routes import *
+from flask import Flask, Response, request
 from flask_cors import CORS
-from routes import autoinfracao, recursos, veiculos, linha
+
+from routes import autoinfracao, recursos, veiculos, linha, consorcio
+from handlers.log import http_logger
+from repositories.database import check_connection
+
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
     # Registra o blueprint 'main'
+    logger.info("::Registering routes::")
     app.register_blueprint(autoinfracao.AutoInfracaoBlueprint)
     app.register_blueprint(recursos.RecursoPrimeiraInstanciaBlueprint)
     app.register_blueprint(veiculos.veiculoBlueprint) 
     app.register_blueprint(linha.linhaBlueprint)
+    app.register_blueprint(consorcio.consorcioBlueprint)
+
+    check_connection()
+
+    @app.after_request
+    def _(response: Response):
+        http_logger.request(request, response.status_code)
+        return response    
 
     return app
--- main.py
+++ main.py
@@ -1,32 +1,16 @@
 from argparse import ArgumentParser
 from waitress import serve
-from flask import Flask
-from flask_cors import CORS
-from routes import autoinfracao, recursos, veiculos, linha, consorcio
-from repositories.database import check_connection
 from handlers.log import logger
 
-logger.info("::Initializing bonfire application::")
+from app import create_app
 
 parser = ArgumentParser()
 parser.add_argument("--debug", action="store_true", help="Debug mode")
 parser.add_argument("--port", type=int, default=5000, help="Server port")
-
 args = parser.parse_args()
 
-app = Flask(__name__)
-CORS(app)
-
-# Registra o blueprint 'main'
-logger.info("::Registering routes::")
-app.register_blueprint(autoinfracao.AutoInfracaoBlueprint)
-app.register_blueprint(recursos.RecursoPrimeiraInstanciaBlueprint)
-app.register_blueprint(recursos.RecuroSegundaInstanciaBlueprint)
-app.register_blueprint(veiculos.veiculoBlueprint) 
-app.register_blueprint(linha.linhaBlueprint)
-app.register_blueprint(consorcio.consorcioBlueprint)
-
-check_connection()
+logger.info("::Initializing bonfire application::")
+app = create_app()
 
 logger.info(f"::Application listening on {args.port}::")
 if args.debug:
